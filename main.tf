5a400bd38953bc64ca4aee216367b871d0232aa8
Naga305 <94951792+Naga305@users.noreply.github.com>
2022-01-10 05:41:23 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ resource "aws_instance" "myFirstInstance" {
   key_name = var.key_name
   instance_type = var.instance_type
   security_groups= [var.security_group]
-  vpc= var.VPC
   tags= {
     Name = var.tag_name
   }
