22ff02fb4d10c9059732a08ea0b802d1bf1b2543
Naga305 <94951792+Naga305@users.noreply.github.com>
2022-01-10 05:08:15 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "My-Serverkye"
+  default     =  "Jenkins-Keypair"
 }
 
 variable "instance_type" {
